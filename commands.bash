cf7e9df4bb7a91c6b609277d3824abaecf1170a8
Maged Saeed <mageedsaeed1@gmail.com>
2024-07-15 15:03:53 +0300

    Update commands.bash
    install vscode

--- commands.bash
+++ commands.bash
@@ -21,3 +21,10 @@ resources: https://gist.github.com/diloabininyeri/637f0353394b95ade468d340f5079a
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
     sudo dpkg -i google-chrome-stable_current_amd64.deb &&
     rm -f google-chrome-stable_current_amd64.deb
+
+:'
+environemnt: gui
+category: mandatory
+title: visual studio code
+'
+sudo snap install code --classic
