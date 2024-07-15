02a9b663287652b8b4d88dd79184bd7535f3af72
Maged Saeed <mageedsaeed1@gmail.com>
2024-07-15 15:30:36 +0300

    Update commands.sh
    better comment messages

--- commands.sh
+++ commands.sh
@@ -6,60 +6,58 @@ is_installed() {
 }
 
 # Check and install git
-:'
+: <<'COMMENT'
 environment: cli
 category: mandatory
 title: git
-'
+COMMENT
+
 if is_installed git; then
     echo "git is already installed."
 else
     sudo apt install -y git
 fi
 
-# Check and install vim
-:'
-environment: cli
-category: mandatory
-title: vim
-'
-if is_installed vim; then
-    echo "vim is already installed."
-else
-    sudo apt install -y vim
-fi
+echo
 
 # Check and install flatpak
-:'
+: <<'COMMENT'
 environment: cli
 category: mandatory
 title: flatpak cli
-'
+COMMENT
+
 if is_installed flatpak; then
     echo "flatpak is already installed."
 else
     sudo apt install -y flatpak
 fi
 
+echo
+
 # Check and install flatpak software gui client
-:'
+: <<'COMMENT'
 environment: gui
 category: mandatory
 title: flatpak software gui client
-'
+COMMENT
+
 if is_installed gnome-software-plugin-flatpak; then
     echo "gnome-software-plugin-flatpak is already installed."
 else
     sudo apt install -y gnome-software-plugin-flatpak
 fi
 
+echo
+
 # Check and install google chrome stable
-:'
+: <<'COMMENT'
 environemnt: gui
 category: mandatory
 title: google chrome stable
 resources: https://gist.github.com/diloabininyeri/637f0353394b95ade468d340f5079ad6
-'
+COMMENT
+
 if is_installed google-chrome-stable; then
     echo "google-chrome-stable is already installed."
 else
@@ -68,26 +66,34 @@ else
     rm -f google-chrome-stable_current_amd64.deb
 fi
 
+echo
+
 # Check and install visual studio code
-:'
+: <<'COMMENT'
 environemnt: gui
 category: mandatory
 title: visual studio code
-'
+COMMENT
+
 if snap list | grep -q code; then
     echo "Visual Studio Code is already installed."
 else
     sudo snap install code --classic
 fi
 
+echo
+
 # Check and install virtualenvwrapper
-:'
-environemnt: cli
+: <<'COMMENT'
+environemnt: gui
 category: mandatory
 title: virtualenvwrapper
-'
+COMMENT
+
 if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
     echo "virtualenvwrapper is already installed."
 else
     sudo chmod +x scripts/virtualenvwrapper.sh && bash scripts/virtualenvwrapper.sh
 fi
+
+echo
