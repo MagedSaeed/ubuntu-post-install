bedf31f5d0b8c25e82bed12fa5e8cc7401a2491e
Maged Saeed <mageedsaeed1@gmail.com>
2024-07-15 15:55:09 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -5,6 +5,11 @@ is_installed() {
     dpkg -s "$1" &> /dev/null
 }
 
+# Function to check if a command is available
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
 # Check and install git
 : <<'COMMENT'
 environment: cli
@@ -12,26 +17,10 @@ category: mandatory
 title: git
 COMMENT
 
-if is_installed git; then
+if command_exists git; then
     echo "git is already installed."
 else
     sudo apt install -y git
-fi
-
-echo
-
-
-# Check and install vim
-: <<'COMMENT'
-environment: cli
-category: mandatory
-title: vim
-COMMENT
-
-if is_installed vim; then
-    echo "vim is already installed."
-else
-    sudo apt install -y vim
 fi
 
 echo
@@ -43,7 +32,7 @@ category: mandatory
 title: flatpak cli
 COMMENT
 
-if is_installed flatpak; then
+if command_exists flatpak; then
     echo "flatpak is already installed."
 else
     sudo apt install -y flatpak
@@ -74,12 +63,27 @@ title: google chrome stable
 resources: https://gist.github.com/diloabininyeri/637f0353394b95ade468d340f5079ad6
 COMMENT
 
-if is_installed google-chrome-stable; then
+if command_exists google-chrome; then
     echo "google-chrome-stable is already installed."
 else
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
     sudo dpkg -i google-chrome-stable_current_amd64.deb &&
     rm -f google-chrome-stable_current_amd64.deb
+fi
+
+echo
+
+# Check and install visual studio code
+: <<'COMMENT'
+environemnt: gui
+category: mandatory
+title: visual studio code
+COMMENT
+
+if snap list | grep -q code; then
+    echo "Visual Studio Code is already installed."
+else
+    sudo snap install code --classic
 fi
 
 echo
@@ -95,6 +99,37 @@ if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
     echo "virtualenvwrapper is already installed."
 else
     sudo chmod +x scripts/virtualenvwrapper.sh && bash scripts/virtualenvwrapper.sh
+fi
+
+echo
+
+# Check and install oh-my-zsh
+: <<'COMMENT'
+environemnt: cli
+category: mandatory
+title: oh-my-zsh
+COMMENT
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "oh-my-zsh is already installed."
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+echo
+
+# Check and install p10k theme
+: <<'COMMENT'
+environemnt: cli
+category: mandatory
+title: powerlevel10k theme
+COMMENT
+
+if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+    echo "Powerlevel10k theme is already installed."
+else
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+    sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' $HOME/.zshrc
 fi
 
 echo
