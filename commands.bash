b2e0f40134046dd09dd8547813ac02527e352bea
Maged Saeed <mageedsaeed1@gmail.com>
2024-07-15 13:45:04 +0300

    Update commands.bash
    install google chrome

--- commands.bash
+++ commands.bash
@@ -1,11 +1,23 @@
 :'
 environment: cli
 category: mandatory
+title: flatpak cli
 '
 sudo apt install -y flatpak
 
 :'
 environment: gui
 category: mandatory
+title: flatpak software gui client
 '
 sudo apt install -y gnome-software-plugin-flatpak
+
+:'
+environemnt: gui
+category: mandatory
+title: google chrome stable
+resources: https://gist.github.com/diloabininyeri/637f0353394b95ade468d340f5079ad6
+'
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+    sudo dpkg -i google-chrome-stable_current_amd64.deb &&
+    rm -f google-chrome-stable_current_amd64.deb
