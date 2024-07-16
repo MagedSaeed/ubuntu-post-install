388f15c81482029a7e306a1458cc86186886b627
Maged Saeed <mageedsaeed1@gmail.com>
2024-07-16 16:07:46 +0300

    Update commands.sh
    add github cli

--- commands.sh
+++ commands.sh
@@ -252,6 +252,26 @@ fi
 
 echo
 
+# Install GitHub CLI
+: <<'COMMENT'
+environment: cli
+category: mandatory
+title: GitHub CLI
+COMMENT
+
+if command_exists gh; then
+    echo "GitHub CLI is already installed."
+else
+    type -p curl >/dev/null || sudo apt install curl -y
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt install gh -y
+fi
+
+echo
+
 # Add aliases to ~/.bin/aliasses.sh
 mkdir -p $HOME/.bin
 cat << 'EOF' > $HOME/.bin/aliasses.sh
